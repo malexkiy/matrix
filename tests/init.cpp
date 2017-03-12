1816bcdc29ef3e2406e3226de21d4d4ef751b609
malexkiy <malexkiy@gmail.com>
2017-03-13 01:31:48 +0300

    Add files via upload

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,100 @@
-#include <matrix.hpp>
+#include <matrix.h>
 #include <catch.hpp>
+#include <sstream>
+#include <cstdlib>
+#include <fstream>
 
 SCENARIO("matrix init", "[init]") {
-	matrix_t matrix;
-	REQUIRE(matrix.rows() == 0);
-	REQUIRE(matrix.columns() == 0);
+	matrix m;
+	REQUIRE(m.rows() == 0);
+	REQUIRE(m.columns() == 0);
+}
+
+SCENARIO("matrix init params", "[init params]") {
+	int r = 3, c = 4;
+	matrix m(r, c);
+	REQUIRE(m.rows() == r);
+	REQUIRE(m.columns() == c);
+}
+
+SCENARIO("matrix fill", "[fill]") {
+	matrix m;
+	ifstream f("a.mtx", std::ios_base::binary);
+	int r, c, *data;
+
+	m.fill("a.mtx");
+
+	f >> r >> c;
+	f.read((char*)data, r * c * sizeof(data[0]));
+	f.close();
+
+	REQUIRE(m.rows() == r);
+	REQUIRE(m.columns() == c);
+
+	for (int i = 0; i < a.rows(); i++)
+	{
+		REQUIRE(memcmp(m.getRow(i + 1), data + i*c, m.columns()*sizeof(int)) == 0);
+	}
+}
+
+SCENARIO("matrix op==", "[op==]") {
+	std::sstream ssa, ssb;
+	matrix a, b;
+
+	ssa << "3 3 1 2 3 4 5 6 7 8 9";
+	ssb << "3 3 1 2 3 4 5 6 7 8 9";
+
+	ssa >> a;
+	ssb >> b;
+	REQUIRE(a.rows() == b.rows());
+	REQUIRE(a.columns() == b.columns());
+	
+	for (int i = 0; i < a.rows(); i++)
+	{
+		REQUIRE(memcmp(a.getRow(i + 1), b.getRow(i + 1), a.columns()*sizeof(int)) == 0);
+	}
+}
+
+SCENARIO("matrix op+", "[op+]") {
+	std::sstream ssa, ssb, ssc;
+	matrix a, b, c;
+
+	ssa << "3 3 1 2 3 4 5 6 7 8 9";
+	ssb << "3 3 10 11 12 13 14 15 16 17 18";
+	ssc << "3 3 11 13 15 17 19 21 23 25 27";
+
+	ssa >> a;
+	ssb >> b;
+	ssc >> c;
+	REQUIRE(a + b == c);
+}
+
+SCENARIO("matrix op*", "[op*]") {
+	std::sstream ssa, ssb, ssc;
+	matrix a, b, c;
+
+	ssa << "3 3 1 2 3 4 5 6 7 8 9";
+	ssb << "3 3 10 11 12 13 14 15 16 17 18";
+	ssc << "3 3 84 90 96 201 216 231 318 342 366";
+
+	ssa >> a;
+	ssb >> b;
+	ssc >> c;
+	REQUIRE(a.columns() == b.rows());
+	REQUIRE(c.rows() == a.rows());
+	REQUIRE(c.columns() == b.columns());
+	REQUIRE(a * b == c);
+}
+
+SCENARIO("matrix op=", "[op=]") {
+	std::sstream ssa;
+	matrix a, b;
+
+	ssa << "3 3 1 2 3 4 5 6 7 8 9";
+
+	ssa >> a;
+	b = a;
+	REQUIRE(a.rows() == b.rows());
+	REQUIRE(a.columns() == b.columns());
+	REQUIRE(a == b);
 }
